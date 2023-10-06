dc8c778f308eb2801823fe1814d3314202ad79fe
Flo700 <florian.martin700@gmail.com>
2023-10-06 16:45:50 +0200

    Mise en place routes, page, composant et sass

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, Outlet} from "react-router-dom";
+
+// Import Pages
+import Home from './page/Home/Home';
+import SignIn from'./page/SignIn/SignIn';
+import User from './page/User/User';
+
+
+// Import Components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function HeaderFooter() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+function router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HeaderFooter />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<SignIn />} />
+          <Route path="/about" element={<User />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default router;
---
+++ frontend/src/page/SignIn/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingIn() {
+  return (
+    <div>signin</div>
+  )
+}
+
+export default SingIn
