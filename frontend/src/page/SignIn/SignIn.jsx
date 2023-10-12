249a04c29a0e132c8cd4380b44a405c2060be139
Flo700 <florian.martin700@gmail.com>
2023-10-12 16:42:36 +0200

    token

--- frontend/src/page/SignIn/SignIn.jsx
+++ frontend/src/page/SignIn/SignIn.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function SignIn() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSignIn = async () => {
+    const response = await fetch('http://localhost:3001/api/v1/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const token = data.body.token;
+      localStorage.setItem('token', token);
+      window.location.href = '/user';
+    } else {
+      alert('Identifiants incorrects.');
+    }
+  };
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
@@ -9,19 +40,36 @@ function SignIn() {
         <h1>Sign In</h1>
         <form>
           <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          <Link to="/user" className="sign-in-button">Sign In</Link>
-          {/* <button className="sign-in-button">Sign In</button> */}
+          <button
+            type="button"
+            className="sign-in-button"
+            onClick={handleSignIn}
+          >
+            Sign In
+          </button>
         </form>
       </section>
     </main>
