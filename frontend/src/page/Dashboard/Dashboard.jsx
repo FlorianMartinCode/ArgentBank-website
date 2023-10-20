c6f2a313d194404600e15c9cf7c149f62d61e934
Flo700 <florian.martin700@gmail.com>
2023-10-20 15:40:18 +0200

    modif dashboard

--- frontend/src/page/Dashboard/Dashboard.jsx
+++ frontend/src/page/Dashboard/Dashboard.jsx
@@ -29,7 +29,7 @@ function Dashboard() {
   return (
     <main className="main bg-dark">
       <div className="header">
-        <h1>Welcome back<br />{userName.userName}</h1>
+      <h1>Welcome back<br />{userName.userName}</h1>
         {!isEditing ? (
           <button className="edit-button" onClick={handleEditClick}>
             Edit Name
@@ -42,19 +42,34 @@ function Dashboard() {
       )}
       <section className='account-content'>
         <section className="account">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+            <p className="account-amount">$2,082.79</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
         </section>
         <section className="account">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+            <p className="account-amount">$10,928.42</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
         </section>
         <section className="account">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+            <p className="account-amount">$184.30</p>
+            <p className="account-amount-description">Current Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
         </section>
       </section>
     </main>
